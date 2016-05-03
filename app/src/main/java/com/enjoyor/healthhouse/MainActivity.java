182fb5082118f6cd2138c14bc12edfd99bab889d
degandroid <degandroid@163.com>
2016-05-03 14:10:25 +0800

    2016-5-3添加启动页以及tab

--- app/src/main/java/com/enjoyor/healthhouse/MainActivity.java
+++ app/src/main/java/com/enjoyor/healthhouse/MainActivity.java
@@ -1,53 +1,29 @@
 package com.enjoyor.healthhouse;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.widget.Toast;
+import android.os.Handler;
 
 import com.enjoyor.healthhouse.ui.BaseActivity;
+import com.enjoyor.healthhouse.ui.MainTabActivity;
 
 public class MainActivity extends BaseActivity {
 
-    /**
-     * 声明一个在按下Back键时的时间
-     */
-    private long preTime;
-    private static final String TAG = "MainActivity";
-
-    //    private String url = "http://115.28.37.145:9008/healthstationserver/health/getrecordnewinfo.action?origin=AndroidApp&userId=35";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setImmerseLayout(findViewById(R.id.common_back));
-        //我给改回来了
-//        User u = new User();
-//        u.setId(1);
-//        u.setName("张鸿洋");
-//
-//        new UserDao(this).add(u);
-//
-//        Log.i("zxw", new UserDao(this).get().getName());
+        Handler x = new Handler();
+        x.postDelayed(new splashhandler(), 2000);
+
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if (System.currentTimeMillis() - preTime > 2000) {
-                Toast.makeText(this, "再按一次返回键退出程序", Toast.LENGTH_SHORT).show();
-            } else {
-                this.finish();
-                System.exit(0);
-            }
-        }
-        return true;
-    }
+    class splashhandler implements Runnable {
 
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
-            preTime = System.currentTimeMillis();
+        public void run() {
+            startActivity(new Intent(getApplication(), MainTabActivity.class));
+            MainActivity.this.finish();
         }
-        return true;
     }
 }
