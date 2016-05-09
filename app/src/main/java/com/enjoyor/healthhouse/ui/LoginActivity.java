36fe53472433452d96a0b5f1f6377c0779e0127d
degandroid <degandroid@163.com>
2016-05-09 14:36:02 +0800

    zxw：添加图片

---
+++ app/src/main/java/com/enjoyor/healthhouse/ui/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.enjoyor.healthhouse.ui;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/5/9.
+ */
+public class LoginActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
