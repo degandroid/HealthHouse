82027c92b6b41ac6d0a0e829f684f47a20001eae
degandroid <degandroid@163.com>
2016-05-17 20:22:26 +0800

    bill

--- app/src/main/java/com/enjoyor/healthhouse/common/Constant.java
+++ app/src/main/java/com/enjoyor/healthhouse/common/Constant.java
@@ -7,4 +7,13 @@ public class Constant {
 
     public static Long accountId;
     public static final String SP_TOKEN = "SP_TOKEN";
+
+    public static final int FROM_XUEYA = 11;
+    public static final int FROM_BMI = 12;
+    public static final int FROM_XUETANG = 13;
+    public static final int FROM_XUEYANG = 14;
+    public static final int FROM_YAOWEI = 21;
+    public static final int FROM_DANGUCHUN = 22;
+    public static final int FROM_NIAOSUAN = 23;
+    public static final int FROM_XINDIAN = 24;
 }
